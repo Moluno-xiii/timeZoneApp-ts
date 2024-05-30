6a6441eea9c9742ef0faf72890f04e49358e4b74
Moluno-xiii <molunoprogress@gmail.com>
2024-05-30 01:48:55 +0100

    added convert time to week

--- src/pages/convertTime/ConvertTimeZones.tsx
+++ src/pages/convertTime/ConvertTimeZones.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import { validTimeZones } from "../../misc/misc";
 import Button from "../../components/Button";
-// import { validTimeZones } from "../../misc/misc";
-// import Button from "../../components/Button";
+import axios from "axios";
+
+interface dataTypes {
+  conversionResult: {
+    date: string;
+    dateTime: string;
+    dstActive: boolean;
+    timeZone: string;
+  };
+  fromTimeZone: string;
+  toTimeZone: string;
+  fromDateTime: string;
+}
+
+const proxyURL = "http://localhost:3000/proxy?url=";
+const url = "https://timeapi.io/api/Conversion/ConvertTimeZone";
 
 const ConvertTimeZones: React.FC = () => {
   const [fromTimeZone, setFromTimeZone] = useState<string>("");
   const [toTimeZone, setToTimeZone] = useState<string>("");
+  const [date, setDate] = useState("2024-05-31");
+  const [time, setTime] = useState("09:40:00");
+  const [data, setData] = useState<dataTypes | undefined>(undefined);
+
+  const dateTime = `${date} ${time}`;
   const handleToTimeZoneChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setToTimeZone(e.target.value);
   };
@@ -15,6 +34,27 @@ const ConvertTimeZones: React.FC = () => {
   ) => {
     setFromTimeZone(e.target.value);
   };
+
+  async function convertTimeZone() {
+    const proxyURL = "http://localhost:3000/proxy?url=";
+    const url = "https://timeapi.io/api/Conversion/ConvertTimeZone";
+    const data = {
+      date,
+    };
+
+    try {
+      const response = await axios.post(`${proxyURL}${url}`, data, {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   return (
     <div className="text-center">
@@ -52,3 +92,55 @@ const ConvertTimeZones: React.FC = () => {
 };
 
 export default ConvertTimeZones;
+
+// import useFetchInfo from "../../hooks/useFetchInfo";
+// import Spinner from "../../components/Spinner";
+// import axios from "axios";
+// import Button from "../../components/Button";
+// import { useState } from "react";
+// import { convertTimeString } from "../../helper/helperFunctions";
+
+// interface dataTypes {
+//   conversionResult: {
+//     date: string;
+//     dateTime: string;
+//     dstActive: boolean;
+//     timeZone: string;
+//   };
+//   fromTimeZone: string;
+//   toTimeZone: string;
+//   fromDateTime: string;
+// }
+
+// const ConvertToWeek: React.FC = () => {
+//   const { isLoading } = useFetchInfo();
+//   const [fromTimeZone, setFromTimeZone] = useState("Africa/Lagos");
+//   const [toTimeZone, setToTimeZone] = useState("America/Los_Angeles");
+//   const [date, setDate] = useState("2024-05-31");
+//   const [time, setTime] = useState("09:40:00");
+//   const [data, setData] = useState<dataTypes | undefined>(undefined);
+
+//   const dateTime = `${date} ${time}`;
+//   async function convertTimeZone() {
+//     const proxyURL = "http://localhost:3000/proxy?url=";
+//     const url = "https://timeapi.io/api/Conversion/ConvertTimeZone";
+//     const data = {
+//       fromTimeZone,
+//       dateTime,
+//       toTimeZone,
+//       dstAmbiguity: "",
+//     };
+
+//     try {
+//       const response = await axios.post(`${proxyURL}${url}`, data, {
+//         headers: {
+//           accept: "application/json",
+//           "Content-Type": "application/json",
+//         },
+//       });
+//       console.log(response.data);
+//       setData(response.data);
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   }
